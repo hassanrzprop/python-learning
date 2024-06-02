865cb0abb74f47c676e68b3239f056a8575914e8
Hassan Raza <141661553+hassanrzprop@users.noreply.github.com>
2024-06-02 17:47:00 +0500

    data types

---
+++ python-data-types.py
@@ -0,0 +1,20 @@
+# string data type
+name:str="hassan raza"
+
+# number data type
+num1:int =23
+num2:float=2.4
+print(num2)
+
+# boolean data type 
+question:bool= True
+
+# list    .It is muteable mean it can be changed later 
+list1:list=[1,2,"ali",["hassan",2.3]]
+
+# tuple are a like list but are mutable and can't be modified later
+tuple1:tuple=(1,2,3,(2,2.3,"hassan"),"ali",3.4)
+
+# dictionary is like a object in typescript aur json data used to store key value pairs 
+dict1:dict={"userName":"hassan123","age":"18"}
+print(dict1)
---
+++ variables.py
@@ -0,0 +1,13 @@
+userName="my name is johnweck.I used to study in Hardward University in USA,California.It is one of the most popular university in the world"
+print(userName)
+
+
+num1,num2,num3=1,2,3
+print(num1,num2,num3)
+
+# to find the index number of the RAM using id attribute
+name="Hassan Raza"
+print(id("Hassan Raza"))    
+
+
+# naming defing ways camel casing(userName),   2) Pascal casing(UserName)    3) snake casing(start with underscore    _userName)
