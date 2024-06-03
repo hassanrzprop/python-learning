d9f80608e7b5e9d57a6274fb108a47b6993387da
Hassan Raza <141661553+hassanrzprop@users.noreply.github.com>
2024-06-03 09:41:46 +0500

    logical operators

--- arithmetic._operations.py
+++ arithmetic._operations.py
@@ -17,3 +17,16 @@ num3=8
 num4=5
 modulasResult=num3% num4
 print(modulasResult)
+
+# exponentiation used to raise power to the other (**)
+number1=2
+number2=3
+res=number1**number2
+print(res)
+
+# float division is used to remove numbers after decimal and convert it to int  symbol is (//)
+
+number3=5
+number4=3
+res1=number3//number4
+print(res1)
---
+++ logical-operators.py
@@ -0,0 +1,42 @@
+#  --------comparison operator----------
+
+#  assign operator is =
+num1=2
+num2=5
+
+#  comparison operator is ==
+#  -equal operator
+result:bool=num1 == num2
+#  not equal operator
+result1= num1 != num2
+print(result1)
+
+# greator than    2) greator than or equal to.    3)less than       4)less than or equal to
+
+print(num1>num2)
+print(num1>=num2)
+print(num1<num2)
+print(num1<=num2)
+
+
+#  --------logical operators----------
+#          ***and*****  all values should be true 
+result2=num2>num1 and num1>num2
+print(result2)
+
+
+#    ****or****  which holds single value true it would be true
+result3=num2>num1 or num1>num2
+print(result3)
+
+# ***not***    reverses the operation of the result
+
+result4= (num1+2>num2 and num2<= 2 or num1**num2)
+print(result4, " expression test")
+
+
+# membership operators is used to find the included find in variable or list there are 2 operators
+userName="Hassan Raza"
+locate="s"
+result5=locate in userName; result6= locate not in userName
+print(result5)
